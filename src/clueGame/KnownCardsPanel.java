fb74ed22dff04c136863557cc1eb5694777b42e0
Will Meinert <whmeinert@mines.edu>
2022-11-11 12:09:19 -0700

    Added base panels for room, person, and weapon cards

--- src/clueGame/KnownCardsPanel.java
+++ src/clueGame/KnownCardsPanel.java
@@ -9,7 +9,7 @@ import javax.swing.border.TitledBorder;
 import javax.swing.text.JTextComponent;
 
 public class KnownCardsPanel extends JPanel {
-    //private final JPanel PERSON;
+    private final JPanel PERSON;
     //private final JPanel ROOM;
     //private final JPanel WEAPON;
     private Board board;
@@ -20,25 +20,26 @@ public class KnownCardsPanel extends JPanel {
         TitledBorder titledBorder = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(0), "Known Cards");
         titledBorder.setTitleJustification(2);
         this.setBorder(titledBorder);
-        /*
+
         this.PERSON = new JPanel();
         this.PERSON.setLayout(new GridLayout(0, 1));
         this.PERSON.setBorder(new TitledBorder(new EtchedBorder(), "People"));
+        /*
         this.ROOM = new JPanel();
         this.ROOM.setLayout(new GridLayout(0, 1));
         this.ROOM.setBorder(new TitledBorder(new EtchedBorder(), "Rooms"));
         this.WEAPON = new JPanel();
         this.WEAPON.setLayout(new GridLayout(0, 1));
         this.WEAPON.setBorder(new TitledBorder(new EtchedBorder(), "Weapons"));
+        */
         this.updatePanels();
         this.add(this.PERSON);
-        this.add(this.ROOM);
-        this.add(this.WEAPON);
-        */
+        //this.add(this.ROOM);
+        //this.add(this.WEAPON);
     }
 
     public final void updatePanels() {
-        //this.updatePanel(this.PERSON, CardType.PERSON);
+        this.updatePanel(this.PERSON, CardType.PERSON);
         //this.updatePanel(this.ROOM, CardType.ROOM);
         //this.updatePanel(this.WEAPON, CardType.WEAPON);
         this.revalidate();
